20f5f453a3c6d51ef5064bfd3a14f29ec0b5c4c6
崔晋瑜 <2510271615@qq.com>
2017-08-24 16:11:29 +0800

    no wait run request

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var url="http://www.xicidaili.com/nn";
 var express = require('express');
 var app = express();
 var proxy=new proxyspider(url);
+proxy.getPage()
+    .then((data)=>{
+        proxy.getIPS(data);
+    }).then(()=>{
+    proxy.check().then(()=>{
+        proxy.write();
+    })
+}).catch((err)=>{
+    console.log(err);
+});
 setInterval(()=>{
     proxy.getPage()
         .then((data)=>{
